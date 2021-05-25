9348502beb83646284cfd38e27743aea4e966352
KarimDerradji <karim.derradji.pro@gmail.com>
2021-05-25 20:43:15 +0200

    from hell with love

--- src/components/GradientsSelect.js
+++ src/components/GradientsSelect.js
@@ -1,7 +1,9 @@
 import { uniqueTags as tags } from "../gradients"
+import { useContext } from "react"
+import { FilterContext } from "./../context/FilterContext"
 
 const GradientsSelect = (props) => {
-  const { filter, setFilter } = props
+  const { filter, setFilter } = useContext(FilterContext)
   const handleSelectChange = (e) => {
     setFilter(e.target.value)
   }
