49435206e765ff4d4bacc08e71cc3113a606dd96
KarimDerradji <karim.derradji.pro@gmail.com>
2021-05-25 16:03:14 +0200

    fetch

--- src/components/Gradients.js
+++ src/components/Gradients.js
@@ -1,9 +1,26 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import GradientsList from "./GradientsList"
 import GradientsSelect from "./GradientsSelect"
 
 const Gradients = () => {
   const [filter, setFilter] = useState("all")
+
+  useEffect(() => {
+    fetch("https://gradients-api.herokuapp.com/gradients")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("something went wrong")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error(error.message)
+      })
+  }, [])
+  
 
   return (
     <>
